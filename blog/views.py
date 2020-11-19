591e370e07cd1dae7f84551c47536e0251d1e92b
Anton <odianboy@mail.ru>
2020-11-19 16:20:10 +0300

    Post creation superuser only, added the author of the post, page pagination, sort by post date, improved administration tab

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,23 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    object_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post_list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_detail(request, pk):
