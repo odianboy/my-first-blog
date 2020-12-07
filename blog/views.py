dd89013e8a8f01c39edb549de13b573801f26fe3
Anton <odianboy@mail.ru>
2020-12-08 00:56:59 +0300

    commented out the tag count line

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,7 @@ def post_detail(request, pk):
 
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-published_date')[:4]
+    # similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-published_date')[:4]
 
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
